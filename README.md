2cfe942d23d3cc2bfa9ae5f422d0485af1289b9a
FeverishPegasus <erikdixon001@gmail.com>
2017-04-15 21:27:54 -0700

    Initial hello world example, updated README

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # CS-470-Final-Project
 Game AI (Checkers Variant)
+
+###Run Halma Board
+python halmaBoard.py
---
+++ halmaBoard.py
@@ -0,0 +1,6 @@
+import tkinter
+from tkinter import *
+screen = tkinter.Tk(className = "Halma GUI")
+status = Label(screen, text = "Hello World")
+status.pack()
+screen.mainloop()
