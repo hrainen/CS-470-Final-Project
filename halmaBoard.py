7cbc12c5b7c8926221788f864c5614f03916e5b7
FeverishPegasus <erikdixon001@gmail.com>
2017-04-16 00:00:54 -0700

    Added quit button

--- halmaBoard.py
+++ halmaBoard.py
@@ -7,12 +7,14 @@ class HalmaGUI:
 		self.screen = screen
 		self.statusText = StringVar()
 		self.statusText.set("The game has begun")
-		self.status = Label(screen, textvariable = self.statusText, justify = CENTER)
-		self.status.grid(row = 0, columnspan = 8)
+		self.status = Label(screen, textvariable = self.statusText, justify = CENTER, relief = RIDGE, width = 25)
+		self.status.grid(row = 0, columnspan = 8, pady = 5)
 		for i in range(8):
 			for j in range(8):
 				tkinter.Button(screen, text = "", command = self.updateLabel, relief = GROOVE)\
 				.grid(row = i+1, column = j, ipadx = 12, ipady = 5, padx = 2, pady = 2)
+		tkinter.Button(screen, text = "QUIT", command = self.quit, relief = GROOVE)\
+		.grid(row = 9, columnspan = 8, ipadx = 15, ipady = 3, pady = 5)
 
 	def updateLabel(self):
 		self.statusText.set("A button has been clicked!")
@@ -20,6 +22,9 @@ class HalmaGUI:
 	
 	def resetLabel(self):
 		self.statusText.set("Back to business")
+		
+	def quit(self):
+		self.screen.destroy()
 
 
 screen = tkinter.Tk(className = "Halma GUI")
