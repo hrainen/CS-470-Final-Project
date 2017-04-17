a0c7691427518af60c33e18a0ec7d8cc375feba3
FeverishPegasus <erikdixon001@gmail.com>
2017-04-17 16:42:03 -0700

    Halma board can now be any size

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 Game AI (Checkers Variant)
 
 ### Run Halma Board
-python halmaBoard.py
+python halmaBoard.py num_rows_cols
--- halmaBoard.py
+++ halmaBoard.py
@@ -1,20 +1,22 @@
 import tkinter
 import time
+import sys
 from tkinter import *
 
 class HalmaGUI:
-	def __init__(self, screen):
+
+	def __init__(self, screen, dim):
 		self.screen = screen
 		self.statusText = StringVar()
 		self.statusText.set("The game has begun")
 		self.status = Label(screen, textvariable = self.statusText, justify = CENTER, relief = RIDGE, width = 25)
-		self.status.grid(row = 0, columnspan = 8, pady = 5)
-		for i in range(8):
-			for j in range(8):
+		self.status.grid(row = 0, columnspan = dim, pady = 5)
+		for i in range(dim):
+			for j in range(dim):
 				tkinter.Button(screen, text = "", command = self.updateLabel, relief = GROOVE)\
 				.grid(row = i+1, column = j, ipadx = 12, ipady = 5, padx = 2, pady = 2)
 		tkinter.Button(screen, text = "QUIT", command = self.quit, relief = GROOVE)\
-		.grid(row = 9, columnspan = 8, ipadx = 15, ipady = 3, pady = 5)
+		.grid(row = dim + 1, columnspan = dim, ipadx = 15, ipady = 3, pady = 5)
 
 	def updateLabel(self):
 		self.statusText.set("A button has been clicked!")
@@ -28,5 +30,5 @@ class HalmaGUI:
 
 
 screen = tkinter.Tk(className = "Halma GUI")
-theGUI = HalmaGUI(screen)
+theGUI = HalmaGUI(screen, int(sys.argv[1]))
 screen.mainloop()
