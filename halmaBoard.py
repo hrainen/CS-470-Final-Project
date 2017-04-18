62d8c0afc4796cad289b01494ab66d3ab1d83b81
FeverishPegasus <erikdixon001@gmail.com>
2017-04-17 20:50:07 -0700

    Added starting piece positions on halma board

--- halmaBoard.py
+++ halmaBoard.py
@@ -6,6 +6,7 @@ from tkinter import *
 class HalmaGUI:
 
 	def __init__(self, screen, dim):
+		self.board = []
 		self.screen = screen
 		self.statusText = StringVar()
 		self.statusText.set("The game has begun")
@@ -13,17 +14,41 @@ class HalmaGUI:
 		self.status.grid(row = 0, columnspan = dim, pady = 5)
 		for i in range(dim):
 			for j in range(dim):
-				tkinter.Button(screen, text = "", command = self.updateLabel, relief = GROOVE)\
-				.grid(row = i+1, column = j, ipadx = 12, ipady = 5, padx = 2, pady = 2)
+				if(i + j + 2 <= dim/2 + 1):
+					self.board.append(['X', self.createButton(i, j, "red")])
+				elif(i + j + 2 >= dim*1.5 + 1):
+					self.board.append(['O', self.createButton(i, j, "blue")])
+				else:
+					self.board.append([' ', self.createButton(i, j, "")])
+				
 		tkinter.Button(screen, text = "QUIT", command = self.quit, relief = GROOVE)\
-		.grid(row = dim + 1, columnspan = dim, ipadx = 15, ipady = 3, pady = 5)
+			.grid(row = dim + 1, columnspan = dim, ipadx = 15, ipady = 3, pady = 5)
 
-	def updateLabel(self):
+	def createButton(self, i, j, color):
+		tempButton = Canvas(self.screen, borderwidth = 2, relief = GROOVE, width = 10, height = 23)
+		tempButton.grid(row = i+1, column = j, ipadx = 12, ipady = 5, padx = 2, pady = 2)
+		tempButton.create_oval(7, 7, 33, 33, fill = color, outline = color)
+		
+		tempButton.bind("<ButtonPress-1>", self.updateLabel)
+		tempButton.bind("<ButtonPress-1>", self.push, add="+")
+		tempButton.bind("<ButtonRelease-1>", self.release)
+		return tempButton
+			
+	def push(self, event):
+		event.widget.configure(relief = SUNKEN)
+	
+	def release(self, event):
+		event.widget.configure(relief = GROOVE)
+	
+	def updateLabel(self, event):
 		self.statusText.set("A button has been clicked!")
 		self.screen.after(1000, self.resetLabel)
 	
 	def resetLabel(self):
 		self.statusText.set("Back to business")
+
+	def refreshBoard(self):
+		pass
 		
 	def quit(self):
 		self.screen.destroy()
