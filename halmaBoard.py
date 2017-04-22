4cd6bcc40112562ee1ba497821febd53392fd86b
FeverishPegasus <erikdixon001@gmail.com>
2017-04-21 19:27:46 -0700

    Added win checking

--- halmaBoard.py
+++ halmaBoard.py
@@ -8,47 +8,48 @@ class HalmaGUI:
 	def __init__(self, screen, dim, inputFile):
 		self.board = []			#This is the list that will hold our halma board
 		self.screen = screen	#This is the window the GUI will go in
-		self.buttonContainer = None
 		self.moveStarted = False
 		self.selectedPiece = None
 		self.movedPieces = [None, None]
+		self.dim = dim
 		self.statusText = StringVar()	#This is the status text at the top of the GUI
-		self.statusText.set("The game has begun")	#Initially set status to "The game has begun"
+		self.statusText.set("Select a piece to move")	#Initially set status to "The game has begun"
 		#Create status text, pass in self.statusText variable
 		self.status = Label(screen, textvariable = self.statusText, justify = CENTER, relief = RIDGE, width = 25)
 		#Add status text to GUI using grid() layout, it should be centered along the top of the halma grid
 		self.status.grid(row = 0, column = 0, pady = 5)
 		
-		if(inputFile == None):
-			self.createBoard(dim)
-		else:
-			self.loadFromFile(inputFile, dim)
+		self.buttonContainer = Canvas(self.screen)
+		self.buttonContainer.grid(row = 1, column = 0)
 		
-		tkinter.Button(screen, text = "SAVE BOARD", command = lambda: self.saveModal(dim), relief = GROOVE)\
+		if(inputFile == None):
+			self.createBoard()
+		else:
+			self.loadFromFile(inputFile)
+		
+		tkinter.Button(screen, text = "SAVE BOARD", command = self.saveModal, relief = GROOVE)\
 			.grid(row = 2, column = 0, ipadx = 15, ipady = 3, pady = 5)
 		
 		#Add quit button to bottom of GUI, centered along halma grid
 		tkinter.Button(screen, text = "QUIT", command = self.quit, relief = GROOVE)\
 			.grid(row = 3, column = 0, ipadx = 15, ipady = 3, pady = 5)
 
-	def createBoard(self, dim):
+	def createBoard(self):
 		#For loop to create buttons that make up halma board (dim^2 total)
-		self.buttonContainer = Canvas(self.screen)
-		self.buttonContainer.grid(row = 1, column = 0)
-		for i in range(dim):
-			for j in range(dim):
-				if(i + j + 2 <= dim/2 + 1):		#If top left corner, create button with red piece
+		for i in range(self.dim):
+			for j in range(self.dim):
+				if(i + j + 2 <= self.dim/2 + 1):		#If top left corner, create button with red piece
 					self.board.append(['X', self.createButton(i, j, "red")])
-				elif(i + j + 2 >= dim*1.5 + 1):	#If bottom right, create button with blue piece
+				elif(i + j + 2 >= self.dim*1.5 + 1):	#If bottom right, create button with blue piece
 					self.board.append(['O', self.createButton(i, j, "blue")])
 				else:							#Else, create blank button (or blank halma square)
 					self.board.append([' ', self.createButton(i, j, "")])
-		self.createWinRegions(dim)
+		self.createWinRegions()
 	
-	def loadFromFile(self, inputFile, dim):
+	def loadFromFile(self, inputFile):
 		j = 0
 		fileID = open(inputFile, 'r')
-		for i in range(dim):
+		for i in range(self.dim):
 			row = fileID.readline().split( )
 			for letter in row:
 				if letter == 'X':
@@ -60,8 +61,9 @@ class HalmaGUI:
 				j += 1
 			j = 0
 		fileID.close()
+		self.createWinRegions()
 	
-	def saveToFile(self, inputModal, outputFile, dim):
+	def saveToFile(self, inputModal, outputFile):
 		i = 0
 		fileID = open(outputFile, 'w')
 		for position in self.board:
@@ -72,7 +74,7 @@ class HalmaGUI:
 			elif position[0] == 'O':
 				fileID.write("O")
 			
-			if i + 1 == dim:
+			if i + 1 == self.dim:
 				i = -1
 				fileID.write("\n")
 			else:
@@ -82,7 +84,7 @@ class HalmaGUI:
 		print("Saved board to: " + outputFile)
 		self.quitModal(inputModal)
 			
-	def saveModal(self, dim):
+	def saveModal(self):
 		saveModal = Toplevel()
 		
 		label = Label(saveModal, text = "File to save: ", justify = LEFT, relief = RIDGE, width = 25)
@@ -91,7 +93,7 @@ class HalmaGUI:
 		entry = Entry(saveModal, justify = LEFT, relief = RIDGE, width = 25)
 		entry.grid(row = 0, column = 1, pady = 5)
 		
-		tkinter.Button(saveModal, text = "SAVE", command = lambda: self.saveToFile(saveModal, entry.get(), dim), relief = GROOVE)\
+		tkinter.Button(saveModal, text = "SAVE", command = lambda: self.saveToFile(saveModal, entry.get()), relief = GROOVE)\
 			.grid(row = 1, column = 0, ipadx = 15, ipady = 3, pady = 5)
 			
 		tkinter.Button(saveModal, text = "CANCEL", command = lambda: self.quitModal(saveModal), relief = GROOVE)\
@@ -101,17 +103,16 @@ class HalmaGUI:
 		saveModal.grab_set()
 		self.screen.wait_window(saveModal)
 	
-	def createWinRegions(self, dim):
+	def createWinRegions(self):
 		#Y start = 45, increment 45
 		#X start = 46, increment 46
-		for i in range(0, dim//2):	#Draw upper left zone
-			self.buttonContainer.create_line(46*i, 45*(dim/2 - i), 46*(i + 1), 45*(dim/2 - i), width = 4)
-			self.buttonContainer.create_line(46*(i + 1), 45*(dim/2 - i), 46*(i + 1), 45*(dim/2 - i - 1), width = 4)
-		for i in range(0, dim//2):	#Draw bottom right zone
-			self.buttonContainer.create_line(46*(dim - i), 45*(dim/2 + i), 46*(dim - i - 1), 45*(dim/2 + i), width = 4)
-			self.buttonContainer.create_line(46*(dim - i - 1), 45*(dim/2 + i), 46*(dim - i - 1), 45*(dim/2 + i + 1), width = 4)
+		for i in range(0, self.dim//2):	#Draw upper left zone
+			self.buttonContainer.create_line(46*i, 45*(self.dim/2 - i), 46*(i + 1), 45*(self.dim/2 - i), width = 4)
+			self.buttonContainer.create_line(46*(i + 1), 45*(self.dim/2 - i), 46*(i + 1), 45*(self.dim/2 - i - 1), width = 4)
+		for i in range(0, self.dim//2):	#Draw bottom right zone
+			self.buttonContainer.create_line(46*(self.dim - i), 45*(self.dim/2 + i), 46*(self.dim - i - 1), 45*(self.dim/2 + i), width = 4)
+			self.buttonContainer.create_line(46*(self.dim - i - 1), 45*(self.dim/2 + i), 46*(self.dim - i - 1), 45*(self.dim/2 + i + 1), width = 4)
 		
-	
 	def createButton(self, i, j, color):	#Create a custom button using Canvas()
 		#Create initial button appearance (does nothing when clicked)
 		tempButton = Canvas(self.buttonContainer, borderwidth = 2, relief = GROOVE, width = 10, height = 23)
@@ -152,17 +153,20 @@ class HalmaGUI:
 				else:				#If space is empty, move selected piece to that location!
 					self.moveSelectedPiece(piece)
 		self.refreshBoard()			#Update board to show changes
-		self.screen.after(1000, self.resetLabel)	#Reset label at top
+	
+	def resetLabel(self):
+		self.statusText.set("Select a piece to move")
 	
 	def moveSelectedPiece(self, piece):	#Moves selectedPiece to the given piece location
 		piece[0] = self.selectedPiece[0]
 		self.selectedPiece[0] = ' '
 		self.movedPieces = [piece, self.selectedPiece]
 		self.selectedPiece = None	#Deselect space, as there are no pieces there anymore
-		self.statusText.set("A piece has been moved!")
-	
-	def resetLabel(self):	#Reset label
-		self.statusText.set("Back to business")
+		if self.gameWon():
+			self.disableBoard()
+		else:
+			self.statusText.set("A piece has been moved!")
+			self.screen.after(2000, self.resetLabel)
 
 	def refreshBoard(self):	#Updates halma piece positions, and visuals
 		for piece in self.board:	#Clear board
@@ -180,6 +184,33 @@ class HalmaGUI:
 				piece[1].create_oval(7, 7, 33, 33, fill = "red", outline = "red")
 			if piece[0] == 'O':
 				piece[1].create_oval(7, 7, 33, 33, fill = "blue", outline = "blue")
+	
+	def gameWon(self):
+		winnerO = True
+		winnerX = True
+		for i in range(self.dim):
+			for j in range(self.dim):
+				if(i + j + 2 <= self.dim/2 + 1):		#If top left corner, create button with red piece
+					if self.board[i * self.dim + j][0] != 'O':
+						winnerO = False
+				elif(i + j + 2 >= self.dim*1.5 + 1):	#If bottom right, create button with blue piece
+					if self.board[i * self.dim + j][0] != 'X':
+						winnerX = False
+		if winnerO and winnerX:
+			self.statusText.set("Both sides won...somehow")
+			return True
+		elif winnerX:
+			self.statusText.set("Team Red Wins!")
+			return True
+		elif winnerO:
+			self.statusText.set("Team Blue Wins!")
+			return True
+		else:
+			return False
+	
+	def disableBoard(self):
+		for button in self.board:
+			button[1].bind("<ButtonPress-1>", self.push)
 	
 	def quitModal(self, inputModal):
 		inputModal.destroy()
